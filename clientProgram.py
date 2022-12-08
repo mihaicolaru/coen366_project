efeab157b2b2ba79f4d0cae54c929279a3f16c9b
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2022-12-07 22:39:16 -0500

    first client push

--- clientProgram.py
+++ clientProgram.py
@@ -1,1 +1,18 @@
 # here is where we write the client side code
+
+import socket
+
+s = socket.socket()
+host = input(str("Please enter the host address of the sender:"))
+port = 10020
+s.connect((host, port))
+print("Client is connected....")
+
+# getting the transfer_file
+filename = input(str(" Enter a filename for the incoming file: "))
+file = open(filename, 'wb')
+file_data = s.recv(1024)
+file.write(file_data)
+print(file_data)
+file.close()
+print(" The file has been received ")

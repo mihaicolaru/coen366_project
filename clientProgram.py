21954901baaf2172ee6d283ebecd2553aa6cf703
unknown <mihai.c.olaru@gmail.com>
2022-12-09 15:05:30 -0500

    add comments

--- clientProgram.py
+++ clientProgram.py
@@ -8,11 +8,39 @@ port = 10020
 s.connect((host, port))
 print("Client is connected....")
 
+# start while(1)
+
+# get user command from std input
+
+# split input string by " "
+
+# parse set of split input string with switch (inputset[0])
+# generate request msg in switch according to command
+
+# put filename
 # getting the transfer_file
 filename = input(str(" Enter a filename for the incoming file: "))
 file = open(filename, 'wb')
+# write file content and size (etc) in msg request
 file_data = s.recv(1024)
 file.write(file_data)
 print(file_data)
 file.close()
 print(" The file has been received ")
+
+# get filename
+
+# change old new
+
+# help
+
+# bye
+# break
+
+# send request msg to server, listen for response
+
+# after response or timeout, loop
+
+
+# after loop, close conntection to server
+

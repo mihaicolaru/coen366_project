539043e983f2625110d7c912b4c9c9296cea74c1
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2022-12-09 18:14:46 -0500

    implemented opcode in parse_input

--- clientProgram.py
+++ clientProgram.py
@@ -1,48 +1,64 @@
 import socket
 
-# open socken
+# open socket
 s = socket.socket()
 host = input(str("Please enter the server IP address: "))
 port = input(str("please enter the server port number: "))
-s.connect((host,int(port)))
+s.connect((host, int(port)))
 print("Client is connected....")
+
+
+# get user command from std input
+def parse_input(input_string):
+    request = ""
+    string_set = input_string.split(" ")
+    if string_set[0] == 'put':
+        print("filename: ", string_set[1])  # put filename
+        request = request + "000"
+        return False, request
+    elif string_set[0] == "get":
+        print("filename: ", string_set[1])  # get filename
+        request = request + "001"
+        return False, request
+    elif string_set[0] == "change":
+        try:
+            print(f"change:\nold filename: {string_set[1]}\nnew filename: {string_set[2]} ")  # change old new
+        except IndexError:
+            print("Filename missing")
+
+        request = request + "010"
+        return False, request
+    elif string_set[0] == "help":
+        print("help")  # help
+        request = request + "011" + "00000"
+        return False, request
+    else:
+        print("connection is going to terminate and exit")  # bye and break/after response or timeout, loop
+        return True, ""
 
 while 1:
     # get user command from std input
     command = input(str("please enter command: "))
-    
-    # split input string by " "
 
+    # split input string by " "
+    terminated, request = parse_input(command)
     # parse set of split input string with switch (inputset[0])
 
     # generate request msg in switch according to command
 
-    # put filename
-    # getting the transfer_file
-    # filename = input(str(" Enter a filename for the incoming file: "))
-    # file = open(filename, 'wb')
-    # write file content and size (etc) in msg request
-    
-
-    # get filename
-
-    # change old new
-
-    # help
-
     # bye
     # break
-    if command == 'bye':
+    if terminated:
         break
 
     # send request msg to server
-    s.send(command.encode())
+    s.send(request.encode())
 
     # listen for response
     response = s.recv(1024)
     print(response.decode())
-    # after response or timeout, loop
-    
+
+
 # after loop, close conntection to server
 s.close()
 print("connection closed")

48ebea61299344fe574d27bd3bd676e178ab3ef7
unknown <mihai.c.olaru@gmail.com>
2022-12-09 21:30:50 -0500

    update

--- serverProgram.py
+++ serverProgram.py
@@ -1,9 +1,16 @@
 import socket
+import sys
 
 # get port number
+try:
+    print("port given: ", sys.argv[1])
+except IndexError:
+    quit("no port given") 
+
 s = socket.socket()
 host = '127.0.0.1'
-port = input(str("enter port server will listen on: "))
+port = sys.argv[1]
+# port = input(str("enter port server will listen on: "))
 s.bind((host,int(port)))
 s.listen(1)
 print(host)
@@ -32,25 +39,39 @@ while 1:
             break
 
         print("request: ",request)
-        # parge request message
-        # 3 first characters: opcode
 
+        # parse request message
+        # 3 first characters: opcode
         opcode = request[0:3]
         print("opcode: ",opcode)
+
         if opcode == '000':
             print("put command")
+
+            print("rest of request: ",request[3:])
+            FL = int("0b" + request[3:8], 2)
+            print("filename length: ",FL)
+
+            file_name = request[8:8+FL]
+            print("filename: ",file_name)
+            
+            FS = int("0b" + request[8+FL+1:8+FL+32], 2)
+            print("file size: ",FS)
+
+            file_data = request[8+FL+32:]
+            print("file data: ",file_data)
+
         elif opcode == '001':
             print("get command")
+            print("rest of request: ",request[3:])
         elif opcode == '010':
             print("change command")
+            print("rest of request: ",request[3:])
         elif opcode == '011':
             print("help command")
         else:
             print("opcode error")
-        # FL = request[3:7]
-        # file_name = request[7:7+FL+1]
         
-        # FS = request[7+FL+2:7+FL+2]
 
 
         # send back response string

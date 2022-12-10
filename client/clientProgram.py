5c80e5a75db2a51d24de850f214af2ad1f05f50b
unknown <mihai.c.olaru@gmail.com>
2022-12-09 21:54:10 -0500

    update

---
+++ client/clientProgram.py
@@ -0,0 +1,156 @@
+import socket
+import sys
+
+#input host number
+try:
+    print("IP address given", sys.argv[1])
+    print("Port given", sys.argv[2])
+except IndexError:
+    quit("No port or IP address given")
+
+try:
+    print("Debug enabled:", sys.argv[3])
+    if sys.argv[3] == 1:
+        debug = True
+    else:
+        debug = False
+except IndexError:
+    quit("debug bit not given")
+
+# open socket
+s = socket.socket()
+host = sys.argv[1]
+port = sys.argv[2]
+s.connect((host, int(port)))
+print("client is connected....")
+
+
+# get user command from std input
+# generate request msg in switch according to command
+def parse_input(input_string):
+    request = ""
+    string_set = input_string.split(" ")
+    if string_set[0] == 'put':
+        # add opcode
+        request = request + "000"
+
+        # try catch to check if argument was passed
+        try:
+            print("filename: ", string_set[1])  # put filename
+        except IndexError:
+            print("Filename not found")  # try open filename, file not found exception just prints file not found
+            return False, "error"
+
+        try:
+            file = open(string_set[1], "r")
+            print("file is open")
+        except Exception:
+            print("file not found")
+            return False, "error"
+
+        # if file is open
+
+        FL = bin(len(string_set[1]))[2:]  # get length of filename, append to request (5 bits)
+        i = 5 - len(FL)
+        while i > 0:
+            request = request + "0"
+            i = i - 1
+        print("FL is ", str(FL))
+        request = request + str(FL)  # append filename length to request
+        request = request + string_set[1]  # append filename to request
+        file_data = file.read()  # read file data into a new string
+        FS = bin(len(file_data))[2:]  # get size of file data string (4 bytes), append to request
+        i = 32 - len(FS)
+        while i > 0:
+            request = request + "0"
+            i = i - 1
+        print("FS is ", str(FS))
+        request = request + str(FS)   # append file data length to request
+        request = request + file_data  # append file data to request
+
+        return True, request
+    elif string_set[0] == "get":
+        # add opcode
+        request = request + "001"
+        # add try catch block to verify that argument was added
+        try:
+            print("filename: ", string_set[1])  # get filename
+        except IndexError:
+            print("File not found")
+            return False, "error"
+
+        FL = bin(len(string_set[1]))[2:] # get length of filename, append to request (5 bits)
+        i = 5 - len(FL)
+        while i > 0:
+            request = request + "0"
+            i = i - 1
+        print("FL is ", str(FL))
+        request = request + str(FL)  # append filename length to request
+        request = request + string_set[1]  # append filename to request
+        return True, request
+    elif string_set[0] == "change":
+        # add opcode
+        request = request + "010"
+
+        try:
+            print(f"change:\nold filename: {string_set[1]}\nnew filename: {string_set[2]} ")  # change old new
+        except IndexError:
+            print("Filename missing")
+            return False, "error"
+
+        old_fl = bin(len(string_set[1]))[2:] # get length of old filename, append to request (5 bits)
+        i = 5 - len(old_fl)
+        while i > 0:
+            request = request + "0"
+            i = i - 1
+        print("FL is ", str(old_fl))
+        request = request + str(old_fl)   # append old filename length to request
+        request = request + string_set[1]  # append old filename to request
+        new_fl = bin(len(string_set[2]))[2:] # get length of new filename, append to request (1 byte)
+        i = 8 - len(new_fl)
+        while i > 0:
+            request = request + "0"
+            i = i - 1
+        print("FL is ", str(new_fl))
+        request = request + str(new_fl)    # append new filename length to request
+        request = request + string_set[2]  # append new filename to request
+
+        return True, request
+    elif string_set[0] == "help":
+        # add opcode
+        request = request + "011" + "00000"
+        print("help")  # help
+
+        return True, request
+    elif string_set[0] == "bye":
+        print("connection is going to terminate and exit")  # bye
+        return False, request
+    else:
+        print("incorrect input")
+        return False, "error"
+
+
+while 1:
+    # get user command from std input
+    command = input(str("please enter command: "))
+
+    # parse command
+    send_request, request = parse_input(command)
+
+    # check if we need to get new command or terminate
+    if not send_request:
+        if request == "error":
+            continue
+        else:
+            break
+
+    # send request msg to server
+    s.send(request.encode())
+
+    # listen for response
+    response = s.recv(1024)
+    print(response.decode())
+
+# after loop, close connection to server
+s.close()
+print("connection closed")
---
+++ server/serverProgram.py
@@ -0,0 +1,104 @@
+import socket
+import sys
+
+# get port number
+try:
+    print("port given: ", sys.argv[1])
+except IndexError:
+    quit("no port given") 
+
+try:
+    print("debug enabled: ", sys.argv[2])
+    if sys.argv[2] == 1:
+        debug = True
+    else:
+        debug = False
+except IndexError:
+    quit("debug bit not given")
+
+s = socket.socket()
+host = '127.0.0.1'
+port = sys.argv[1]
+# port = input(str("enter port server will listen on: "))
+s.bind((host,int(port)))
+s.listen(1)
+print(host)
+
+while 1:
+    print("waiting for clients")
+    connection, address = s.accept()
+    print(address," has been connected to the server")
+    connected = True
+
+    while connected:
+        # listen for request messages from client, in try catch block in case connection broken
+        try:
+            request = connection.recv(1024).decode()
+        except BlockingIOError:
+            print("reading")
+        except ConnectionResetError:
+            print("connection was reset")
+            break
+        except BrokenPipeError:
+            print("connection was broken")
+            break
+
+        if len(request) == 0:
+            print("no data")
+            break
+
+        print("request: ",request)
+
+        # parse request message
+        # 3 first characters: opcode
+        opcode = request[0:3]
+        print("opcode: ",opcode)
+
+        if opcode == '000':
+            print("put command")
+
+            print("rest of request: ",request[3:])
+
+            FL = int("0b" + request[3:8], 2)
+            print("filename length: ",FL)
+
+            file_name = request[8:8+FL]
+            print("filename: ",file_name)
+            
+            FS = int("0b" + request[8+FL:8+FL+32], 2)
+            print("file size: ",FS)
+
+            file_data = request[8+FL+32:]
+            print("file data: ",file_data)
+
+        elif opcode == '001':
+            print("get command")
+            print("rest of request: ",request[3:])
+        elif opcode == '010':
+            print("change command")
+            print("rest of request: ",request[3:])
+        elif opcode == '011':
+            print("help command")
+        else:
+            print("opcode error")
+        
+
+
+        # send back response string
+        response = 'received: ' + request
+        try:
+            connection.send(response.encode())
+        except ConnectionResetError:
+            print("connection was reset")
+            break
+        except BrokenPipeError:
+            print("connection was broken")
+            break
+        except ConnectionAbortedError:
+            print("connection was aborted")
+            break
+
+        # if connection broken, go back to listening for connections
+
+print("server terminating")
+connection.close()

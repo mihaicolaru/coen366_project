97e88cf57b5ad27d81cdf114d20bf86ba996b859
unknown <mihai.c.olaru@gmail.com>
2022-12-09 19:18:22 -0500

    updated

--- clientProgram.py
+++ clientProgram.py
@@ -5,51 +5,83 @@ s = socket.socket()
 host = input(str("Please enter the server IP address: "))
 port = input(str("please enter the server port number: "))
 s.connect((host, int(port)))
-print("Client is connected....")
+print("client is connected....")
 
 
 # get user command from std input
+# generate request msg in switch according to command
 def parse_input(input_string):
     request = ""
     string_set = input_string.split(" ")
     if string_set[0] == 'put':
-        print("filename: ", string_set[1])  # put filename
+        # add opcode
         request = request + "000"
-        return False, request
+
+        # try catch to check if argument was passed
+        print("filename: ", string_set[1])  # put filename
+
+        # try open filename, file not found exception just prints file not found
+
+        # if file is open
+        # get length of filename, append to request (5 bits)
+        # append filename to request
+        # read file data into a new string
+        # get size of file data string (4 bytes), append to request
+        # append file data to request        
+        
+        return True, request
     elif string_set[0] == "get":
-        print("filename: ", string_set[1])  # get filename
+        # add opcode
         request = request + "001"
-        return False, request
+        # add try catch block to verify that argument was added
+        print("filename: ", string_set[1])  # get filename
+        
+        # get length of filename, append to request (5 bits)
+        # append filename to request
+
+        return True, request
     elif string_set[0] == "change":
+        # add opcode
+        request = request + "010"
+
         try:
             print(f"change:\nold filename: {string_set[1]}\nnew filename: {string_set[2]} ")  # change old new
         except IndexError:
             print("Filename missing")
-
-        request = request + "010"
-        return False, request
+            return False, "error"
+        
+        # get length of old filename, append to request (5 bits)
+        # append old filename to request
+        # get length of new filename, append to request (1 byte)
+        # append new filename to request
+        
+        return True, request
     elif string_set[0] == "help":
-        print("help")  # help
+        # add opcode
         request = request + "011" + "00000"
+        print("help")  # help
+        
+        return True, request
+    elif string_set[0] == "bye":
+        print("connection is going to terminate and exit")  # bye
         return False, request
     else:
-        print("connection is going to terminate and exit")  # bye and break/after response or timeout, loop
-        return True, ""
+        print("incorrect input")
+        return False, "error"
 
 while 1:
     # get user command from std input
     command = input(str("please enter command: "))
 
-    # split input string by " "
-    terminated, request = parse_input(command)
-    # parse set of split input string with switch (inputset[0])
-
-    # generate request msg in switch according to command
-
-    # bye
-    # break
-    if terminated:
-        break
+    # parse command
+    send_request, request = parse_input(command)
+    
+    # check if we need to get new command or terminate
+    if not send_request:
+        if request == "error":
+            continue
+        else:
+            break
 
     # send request msg to server
     s.send(request.encode())
